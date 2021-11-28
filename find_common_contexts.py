472f25434042775314ff8ef95b9a363b61b7279c
andkasia <82385993+andkasia@users.noreply.github.com>
2021-11-28 11:45:10 +0100

    Add files via upload

---
+++ find_common_contexts.py
@@ -0,0 +1,37 @@
+#how to find common contexts of pairs of words at the example of all texts from nltl Book
+
+print('')
+print('''Common contexts for the words 'sea' and 'man' in text 1:''')
+text1.common_contexts(["sea", "man"])
+print('')
+
+print('''Common contexts for the words 'beautiful' and 'pretty' in text 2:''')
+text2.common_contexts(["beautiful", "pretty"])
+print('')
+
+print('''Common contexts for the words 'god' and 'earth' in text 3:''')
+text3.common_contexts(["god", "earth"])
+print('')
+
+print('''Common contexts for the words 'democracy' and 'peace' in text 4:''')
+text4.common_contexts(["democracy", "peace"])
+print('')
+
+print('''Common contexts for the words 'hello' and 'hi' in text 5:''')
+text5.common_contexts(["hello", "hi"])
+print('')
+
+print('''Common contexts for the words 'quest' and 'grail' in text 6:''')
+text6.common_contexts(["quest", "grail"])
+print('')
+
+print('''Common contexts for the words 'sell' and 'buy' in text 7:''')
+text7.common_contexts(["sell", "buy"])
+print('')
+
+print('''Common contexts for the words 'i' and 'we' in text 8:''')
+text8.common_contexts(["i", "we"])
+print('')
+
+print('''Common contexts for the words 'policeman' and 'man' in text 9:''')
+text9.common_contexts(["policeman", "man"])
